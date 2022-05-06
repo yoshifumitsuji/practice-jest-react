26648f390b8705d7739dc0129ea3d54863ad7683
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-05-06 19:56:43 +0900

    Practice getAllByText

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Hello</p>
+    </>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,13 @@ import App from './App';
 
 test('renders Hello', () => {
   render(<App />);
+  // 複数の要素があるので、getBy... エラーになる。
   // const element = screen.getByText('Hello');
-  // const element = screen.getByText('ello');
-  const element = screen.queryByText('ello');
-  screen.debug(element);
+  // 複数の要素があるのでがある場合は getAll... を使用する。
+  const elements = screen.getAllByText('Hello');
+  screen.debug(elements);
+  // getAllBy は配列の形になるので toBeInTheDocument は使えない。
   // expect(element).toBeInTheDocument();
-  expect(element).not.toBeInTheDocument();
+  // getAllBy を使った場合は toHaveLength を使う。
+  expect(elements).toHaveLength(2);
 });
