c03fbafaf1f674cbe66224c95ec1b5f20a872a8d
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-05-06 19:11:53 +0900

    practice queryByText

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,10 @@ import App from './App';
 
 test('renders Hello', () => {
   render(<App />);
-  const element = screen.getByText('Hello');
+  // const element = screen.getByText('Hello');
+  // const element = screen.getByText('ello');
+  const element = screen.queryByText('ello');
   screen.debug(element);
-  expect(element).toBeInTheDocument();
+  // expect(element).toBeInTheDocument();
+  expect(element).not.toBeInTheDocument();
 });
